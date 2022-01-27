dac1c07522c11b1b28d0a1c17eb16109e2e54293
aorticweb <aorticweb@aorticweb.com>
2022-01-27 13:37:15 -0500

    db columns renaming

--- db/migrations/20220125181413_init.sql
+++ db/migrations/20220125181413_init.sql
@@ -5,19 +5,19 @@ create table if not exists public.user (
 );
 create table if not exists public.group (
     id SERIAL primary key,
-    name VARCHAR(240)
+    groupname VARCHAR(240)
 );
 create table if not exists user_group (
     id SERIAL primary key,
     user_id int references public.user(id) not null,
     group_id int references public.group(id) not null,
-    UNIQUE (user_id, group_id)
+    CONSTRAINT unique_user_group UNIQUE (user_id, group_id)
 );
 create table if not exists message (
     id SERIAL primary key,
     re_id int references message(id),
-    sender int references public.user(id) not null,
-    recipient int references public.user(id) not null,
+    sender_id int references public.user(id) not null,
+    recipient_id int references public.user(id) not null,
     subject text,
     body text,
     sent_at timestamp without time zone not null
