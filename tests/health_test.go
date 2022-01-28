8699ee26b72cfe44f4230761fa7ed2f953b342ee
aorticweb <aorticweb@aorticweb.com>
2022-01-28 13:00:38 -0500

    re-enable health test

--- tests/health_test.go
+++ tests/health_test.go
@@ -1,13 +1,20 @@
 package tests
 
-// func TestHealth(t *testing.T) {
-// 	db := testDB(t)
-// 	srv := testServer(t, db)
-// 	defer clean(t, db, srv)
+import (
+	"net/http"
+	"testing"
 
-// 	resp, err := http.Get(url(srv.URL, "/health"))
+	"github.com/stretchr/testify/require"
+)
 
-// 	require.NoError(t, err)
-// 	require.Equal(t, resp.StatusCode, http.StatusOK)
-// 	checkContentType(t, resp)
-// }
+func TestHealth(t *testing.T) {
+	db := testDB(t)
+	srv := testServer(t, db)
+	defer clean(t, db, srv)
+
+	resp, err := http.Get(url(srv.URL, "/health"))
+
+	require.NoError(t, err)
+	require.Equal(t, resp.StatusCode, http.StatusOK)
+	checkContentType(t, resp)
+}
